842a819dfdb253a3da4f2b93093b9b17864d70a3
Nestor Júnio <junionestor@hotmail.com>
2024-04-18 20:58:36 -0300

    feat: user repository e base eloquent repository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,10 +4,11 @@ namespace App\Repositories;
 
 use App\Models\User;
 use App\Repositories\Contracts\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
 
-class UserRepository  extends AbstractEloquentRepository implements UserRepositoryInterface
+class UserRepository extends AbstractEloquentRepository implements UserRepositoryInterface
 {
-    public function resolveModel()
+    public function resolveModel(): Model
     {
         return app(User::class);
     }
